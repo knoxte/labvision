6bd5121c415488b02e22f96f93d0671fcf8f1b5d
James <jamesdownslab@gmail.com>
2020-01-29 14:42:34 +0000

    Setup initial structure

--- setup.py
+++ setup.py
@@ -3,10 +3,17 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("LICENSE", "r") as fh:
+    license = fh.read()
+
 setuptools.setup(
     name='labvision',
     version='0.1',
-    license='MIT',
-    packages=setuptools.find_packages(),
+    license=license,
+    packages=setuptools.find_packages(
+        exclude=('tests', 'docs')
+    ),
+    url='https://github.com/MikeSmithLabTeam/labvision',
+    
 )
 
