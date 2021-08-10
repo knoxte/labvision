c1fa0d8dcce8f5e8cbe33886a472506adfd3d3b2
ppypn4 <ppypm4@nottingham.ac.uk>
2021-08-10 14:55:24 +0100

    changed test

--- labvision/camera/tests/test_panasonic_camera.py
+++ labvision/camera/tests/test_panasonic_camera.py
@@ -6,12 +6,14 @@ import time
 import subprocess
 import os
 import cv2
+#
+# cam1 = camera.Panasonic()
+# cam1.take_frame()
+# filename = cam1.save_file_onto_computer()
+# print(filename)
 
-cam1 = camera.Panasonic()
-cam1.take_frame()
-filename = cam1.save_file_onto_computer()
-print(filename)
 
-
-# cam2 = camera.Panasonic(mode='Movie')
-# cam2.list_files()
+cam2 = camera.Panasonic(mode='Movie')
+cam2.delete_multiple_files_from_camera()
+cam2.start_movie(duration=3)
+cam2.save_file_onto_computer()
