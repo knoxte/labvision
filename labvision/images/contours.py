b5088fdc45b8633f9404e2ab029c335ef9bb6c53
MikeSmithLab <mikesmithlab@gmail.com>
2023-09-21 18:37:54 +0100

    pass tests add conda env file

--- labvision/images/contours.py
+++ labvision/images/contours.py
@@ -40,13 +40,9 @@ def find_contours(img : np.ndarray, hierarchy : bool=False):
                         [[x,y]], dtype=int32)]
     """
     
-    # work for any version of opencv
-    try:
-        im, contours, hier = cv2.findContours(
+    contours, hier = cv2.findContours(
             img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    except:
-        contours, hier = cv2.findContours(
-            img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
     if hierarchy:
         return contours, hier
     else:
